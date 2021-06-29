16a7bfc6a8e036013d807b702c31cd26f0abed27
Kate Quinn <katequinn7@gmail.com>
2021-06-29 17:39:21 -0400

    added app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+const App = (props) => {
+  return (
+    <div>
+      <p>Hello there!</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
