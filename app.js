2dac17545d84ac9194c46d64a6ed924ae1886ce8
Kate Quinn <katequinn7@gmail.com>
2022-08-31 12:30:06 -0400

    updating projects, css

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav, Carousel } from 'react-bootstrap';
-import { Router, Link } from 'react-router-dom';
+// import { Router, Link } from 'react-router-dom';
 import './css/App.css';
 
-const App = (props) => {
+const App = () => {
   return (
     <div id="main">
       <Navbar fixed="top" expand="lg">
@@ -45,16 +45,6 @@ const App = (props) => {
               >
                 PROJECTS
               </Nav.Link>
-              {/* <Nav.Link
-                style={{
-                  margin: 'auto .5rem',
-                  color: 'white',
-                  fontSize: '1.5rem',
-                }}
-                href="/resume"
-              >
-                Resume
-              </Nav.Link> */}
               <Nav.Link
                 style={{
                   margin: 'auto .5rem',
@@ -83,59 +73,76 @@ const App = (props) => {
         <div>
           <h2>About Me</h2>
           <p>
-            As a pysical therapist turned software engineer, I'm taking a love
-            of problem-solving from humans to code. I love the idea of lifelong
-            learning, and I look forward to my next project pushing that even
-            futher.
+            As a pysical therapist turned software engineer, I've taken a love
+            of problem-solving from humans to code.
+          </p>
+          <p>
+            I'm commited to lifelong learning, and I look forward to my next
+            project pushing that even futher.
           </p>
         </div>
       </section>
       <section id="projects">
         <h2>Projects</h2>
-        <div id="project-wrapper">
-          {projects.map((project) => (
-            <div>
-              <h3>{project.title}</h3>
-              <p>{project.description}</p>
-              <div>
-                <a href={project.liveUrl}>
-                  {project.images.length > 1 ? (
-                    <Carousel variant="dark">
-                      {project.images.map((imgSrc) => (
-                        <Carousel.Item>
-                          <img className="d-block w-100" src={imgSrc} />
-                        </Carousel.Item>
-                      ))}
-                    </Carousel>
-                  ) : (
-                    <img src={project.images[0]}></img>
-                  )}
-                </a>
+        <hr></hr>
+        {Object.keys(projects).map((projectType, index) => (
+          <>
+            <div key={index}>
+              <h2>{projectType}</h2>
+              <div id="project-wrapper">
+                {projects[projectType].map((project, idx) => (
+                  <div id="project-info" key={idx}>
+                    <h2>{project.title}</h2>
+                    <p>{project.description}</p>
+                    <div>
+                      <a href={project.liveUrl}>
+                        {project.images.length > 1 ? (
+                          <Carousel variant="dark">
+                            {project.images.map((imgSrc) => (
+                              <Carousel.Item>
+                                <img className="d-block w-100" src={imgSrc} />
+                              </Carousel.Item>
+                            ))}
+                          </Carousel>
+                        ) : (
+                          <img src={project.images[0]}></img>
+                        )}
+                      </a>
+                    </div>
+                    {project.liveUrl ? (
+                      <>
+                        <p>
+                          <a href={project.liveUrl}>view live site</a>
+                        </p>
+                      </>
+                    ) : (
+                      ''
+                    )}
+                    {project.githubUrl ? (
+                      <p>
+                        <a href={project.githubUrl}>
+                          view source code on github
+                        </a>
+                      </p>
+                    ) : (
+                      ''
+                    )}
+
+                    {project.videoUrl ? (
+                      <p>
+                        <a href={project.videoUrl}>video presentation</a>
+                      </p>
+                    ) : (
+                      ''
+                    )}
+                  </div>
+                ))}
+                <hr></hr>
               </div>
-              {project.liveUrl ? (
-                <>
-                  <p>
-                    <a href={project.liveUrl}>view live site</a>
-                  </p>
-                </>
-              ) : (
-                ''
-              )}
-
-              <p>
-                <a href={project.githubUrl}>view source code on github</a>
-              </p>
-
-              {project.videoUrl ? (
-                <p>
-                  <a href={project.videoUrl}>video presentation</a>
-                </p>
-              ) : (
-                ''
-              )}
             </div>
-          ))}
-        </div>
+            <hr></hr>
+          </>
+        ))}
       </section>
     </div>
   );
--- projectData.js
+++ projectData.js
@@ -1,38 +1,93 @@
-export const projects = [
-  {
-    title: 'Q BOOK CLUBS',
-    description:
-      'Solo project: Inspired by Amazon Book Clubs, but with additional user features',
-    images: ['/images/qbshot.png'],
-    githubUrl: 'https://github.com/KQ77/reads-and-weeds.git',
-    liveUrl: 'http://q-bookclubs.herokuapp.com/',
-  },
-  {
-    title: 'PUZZLE BREAK',
-    description:
-      'Group Project: Design a custom, virtual escape room, or play a pre-made game',
-    images: ['/images/puzzlethumbnail.png'],
-    githubUrl: 'https://github.com/escape-room-capstone/escape-room.git',
-    liveUrl: '',
-    videoUrl: 'https://youtu.be/qO-k2z-BG_4',
-  },
-  {
-    title: 'Q-BOT: THE SLACK BOT WHO CARES',
-    description:
-      'Solo 6-day hackathon: custom slack bot with health & wellness in mind - integrates with FitBot',
-    images: [
-      '/images/slackbot.png',
-      '/images/botshot2.png',
-      '/images/botshot3.png',
-      '/images/commandlist.png',
-    ],
-    githubUrl: 'https://github.com/KQ77/Q-Bot.git',
-  },
-  {
-    title: `KAVAKORO'S PLANT SHOP`,
-    description: 'Early group project: E-commerce site selling house plants',
-    images: ['/images/kavakoro.png', '/images/kavacart.png'],
-    liveUrl: 'kavakoro.com',
-    githubUrl: 'https://github.com/Kavakoro/plant-store.git',
-  },
-];
+export const projects = {
+  Professional: [
+    {
+      title: 'AUTOBOT',
+      description:
+        'PRICELINE.COM Internal Tool Used To Run Automated Tests (sole front-end developer, and assisted with backend API)',
+      images: [
+        '/images/autobot1.png',
+        '/images/autobot3.png',
+        '/images/autobot4.png',
+        '/images/autobot5.png',
+        '/images/autobot6.png',
+        '/images/autobot7.png',
+      ],
+    },
+  ],
+  Personal: [
+    {
+      title: 'Q BOOK CLUBS',
+      description:
+        'Solo project: Inspired by Amazon Book Clubs, but with additional user features',
+      images: ['/images/qbshot.png'],
+      githubUrl: 'https://github.com/KQ77/reads-and-weeds.git',
+      liveUrl: 'http://q-bookclubs.herokuapp.com/',
+    },
+    {
+      title: 'PUZZLE BREAK',
+      description:
+        'Group Project: Design a custom, virtual escape room, or play a pre-made game',
+      images: ['/images/puzzlethumbnail.png'],
+      githubUrl: 'https://github.com/escape-room-capstone/escape-room.git',
+      videoUrl: 'https://youtu.be/qO-k2z-BG_4',
+    },
+    {
+      title: 'Q-BOT: THE SLACK BOT WHO CARES',
+      description:
+        'Solo 6-day hackathon: custom slack bot with health & wellness in mind - integrates with FitBot',
+      images: [
+        '/images/slackbot.png',
+        '/images/botshot2.png',
+        '/images/botshot3.png',
+        '/images/commandlist.png',
+      ],
+      githubUrl: 'https://github.com/KQ77/Q-Bot.git',
+    },
+    {
+      title: `KAVAKORO'S PLANT SHOP`,
+      description: 'Early group project: E-commerce site selling house plants',
+      images: ['/images/kavakoro.png', '/images/kavacart.png'],
+      liveUrl: 'kavakoro.com',
+      githubUrl: 'https://github.com/Kavakoro/plant-store.git',
+    },
+  ],
+};
+
+// export const projects = [
+//   {
+//     title: 'Q BOOK CLUBS',
+//     description:
+//       'Solo project: Inspired by Amazon Book Clubs, but with additional user features',
+//     images: ['/images/qbshot.png'],
+//     githubUrl: 'https://github.com/KQ77/reads-and-weeds.git',
+//     liveUrl: 'http://q-bookclubs.herokuapp.com/',
+//   },
+//   {
+//     title: 'PUZZLE BREAK',
+//     description:
+//       'Group Project: Design a custom, virtual escape room, or play a pre-made game',
+//     images: ['/images/puzzlethumbnail.png'],
+//     githubUrl: 'https://github.com/escape-room-capstone/escape-room.git',
+//     liveUrl: '',
+//     videoUrl: 'https://youtu.be/qO-k2z-BG_4',
+//   },
+//   {
+//     title: 'Q-BOT: THE SLACK BOT WHO CARES',
+//     description:
+//       'Solo 6-day hackathon: custom slack bot with health & wellness in mind - integrates with FitBot',
+//     images: [
+//       '/images/slackbot.png',
+//       '/images/botshot2.png',
+//       '/images/botshot3.png',
+//       '/images/commandlist.png',
+//     ],
+//     githubUrl: 'https://github.com/KQ77/Q-Bot.git',
+//   },
+//   {
+//     title: `KAVAKORO'S PLANT SHOP`,
+//     description: 'Early group project: E-commerce site selling house plants',
+//     images: ['/images/kavakoro.png', '/images/kavacart.png'],
+//     liveUrl: 'kavakoro.com',
+//     githubUrl: 'https://github.com/Kavakoro/plant-store.git',
+//   },
+// ];
